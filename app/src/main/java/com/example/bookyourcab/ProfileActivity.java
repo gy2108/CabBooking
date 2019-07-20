45069c6c688fbc120ba0100398640faad1277478
gy2108 <gopalyadav2108@gmail.com>
2019-07-20 22:00:39 +0530

    SqliteDb

--- app/src/main/java/com/example/bookyourcab/ProfileActivity.java
+++ app/src/main/java/com/example/bookyourcab/ProfileActivity.java
@@ -11,8 +11,9 @@ import android.widget.Toast;
 public class ProfileActivity extends Activity {
 
     TextView name, email, phone;
-    SharedPreferences sharedPreferences;
+//    SharedPreferences sharedPreferences;
     SQLiteDatabase  sqLiteDatabase;
+    MyDBHelper myDBHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +24,17 @@ public class ProfileActivity extends Activity {
         email = findViewById(R.id.email);
         phone = findViewById(R.id.phone_number);
 
+        myDBHelper=new MyDBHelper(this);
+        sqLiteDatabase=myDBHelper.getWritableDatabase();
+
         Cursor cur = sqLiteDatabase.query(MyDBHelper.TABLE_NAME, new String[]{MyDBHelper.EMP_NAME, MyDBHelper.EMP_EMAIL,
-                    MyDBHelper.EMP_PHONE}, MyDBHelper.EMP_NAME+"=?", new String[]{"Gopal"},
+                    MyDBHelper.EMP_PHONE}, null, null,
                 null, null, null);
         cur.moveToFirst();
 
-        /*name.setText(cur.getString(0));
+        name.setText(cur.getString(0));
         email.setText(cur.getString(1));
-        phone.setText(cur.getString(2));*/
+        phone.setText(cur.getString(2));
 
         while(!cur.isAfterLast()){
             Toast.makeText(this,"Name  is  "+cur.getString(0)+ "  and Email is  "+
