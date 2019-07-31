f31fbdd80afa7e5ec4b5ce79c500711417bde3ea
gy2108 <gopalyadav2108@gmail.com>
2019-08-01 01:39:21 +0530

    Grid view image changes

--- app/src/main/java/com/example/bookyourcab/CustomAdapter.java
+++ app/src/main/java/com/example/bookyourcab/CustomAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -31,6 +32,7 @@ public class CustomAdapter extends ArrayAdapter {
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         v = inflater.inflate(R.layout.gridview, null);
         ImageView imageView = v.findViewById(R.id.grid_image);
+        imageView.setLayoutParams(new LinearLayout.LayoutParams(300,300));
         imageView.setImageResource((int)iconList.get(position));
         return v;
 
